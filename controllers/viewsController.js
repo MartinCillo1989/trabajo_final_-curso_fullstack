ba6e07efd61e5c9ddd369dc37671960b4dee2952
Martin Cillo <cillomartin.89@gmail.com>
2023-12-09 01:41:53 -0300

    ultimando detalles

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,3 +1,6 @@
+const Product = require('../models/productsModels')
+
+
 const login=(req,res)=>{
     res.render('pages/login');
 }
@@ -6,9 +9,10 @@ const register=(req,res)=>{
     res.render('pages/register');
 }
 
-const home = (req,res)=>{
+const home = async (req,res)=>{
+    const products = await Product.find({})
     if(req.user)
-    {res.render('pages/home.ejs',{user:req.user})}
+    {res.render('pages/home.ejs',{user:req.user,products:products})}
     else{
         res.redirect('http://localhost:3000/login')
     }
